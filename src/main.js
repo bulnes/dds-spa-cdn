296609bfab70a96e93ae6ddd9268b7e50938f4f4
Bruno Bulnes <bulnes.bruno@gmail.com>
2023-03-10 21:51:01 -0300

    chore: remove public folder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import navigation from "./router";
 import { configSpaLinks } from "./utilities/configSpaLinks";
 import { initSideNav } from "./utilities/initSideNav";
+
 import "./assets/styles/main.scss";
 
 navigation(window.location.pathname);
